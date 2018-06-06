3fc5fb2494fa1677f43f1dc2da749a2696fcf3f3
Clint Hall <bclinthall@gmail.com>
2018-06-06 11:23:14 -0400

    show variants

--- lexical_stress_service.py
+++ lexical_stress_service.py
@@ -1,16 +1,17 @@
 import prosodic as p
 from flask import Flask, request
+showAlternatives = True
 
 app = Flask(__name__)
 
 
 def mark_word_variants(word_variants):
-    for word in word_variants:
-        if word.isMonoSyllab():
-            return [word.getToken().upper()]
-        else:
-            return [mark_word(word) for word in word_variants]
+    if len(word_variants) == 1 or isMonoSyllab(word_variants):
+        return mark_word(word_variants[0])
+    return str([mark_word(word) for word in word_variants])
 
+def isMonoSyllab(word_variants):
+    return all([word.isMonoSyllab() for word in word_variants])
 
 def mark_word(word):
     if word.isMonoSyllab():
@@ -24,8 +25,11 @@ def mark_syllable(syllable):
 
 def mark_line(line):
     t = p.Text(line)
-    words = t.words(flattenList=True)
-    return " ".join([mark_word(word) for word in words])
+    words = t.words(flattenList=(not showAlternatives))
+    if showAlternatives:
+        return " ".join([mark_word_variants(word_variants) for word_variants in words])
+    else:
+        return " ".join([mark_word(word) for word in words])
     
 
 @app.route("/", methods=['POST'])
