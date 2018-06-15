df1704466ea57fb97489c0ff5ba079994d6f3c80
Clint Hall <bclinthall@gmail.com>
2018-06-15 03:37:05 +0000

    update test for new url

--- testLexicalStressService.sh
+++ testLexicalStressService.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget --header='Content-Type: text/plain' --post-file=mary.txt http://localhost:5121 -O - -nv
+wget --header='Content-Type: text/plain' --post-file=mary.txt http://198.211.105.27:5121 -O - -nv
